a00023aed443ce438f060fe25527b4a65d9bd29c
HernanFAR <h.f.alvarez.r@gmail.com>
2020-09-19 01:10:49 -0300

    Se agregaron las librerias Vuelidate y VueSweetAlert2

--- src/main.js
+++ src/main.js
@@ -4,7 +4,18 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import Vuelidate from "vuelidate";
+
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+import VueSweetalert2 from "vue-sweetalert2";
+
 Vue.config.productionTip = false;
+
+Vue.use(Vuelidate);
+Vue.use(VueAxios, axios);
+Vue.use(VueSweetalert2);
 
 new Vue({
   router,
